cb73b263a468d6c2fad6ce5734c11a1593aa52f7
aidancgray <aidancgray@gmail.com>
2021-03-18 21:40:36 +0000

    testing '

--- python/main.py
+++ python/main.py
@@ -6,12 +6,10 @@ import time
 def main():
     eeprom = EEPROM(0x50)
 
-    read1 = eeprom.read(0, 0, 8)
-    eeprom.write(0, 0, [0x00, 0x00, 0x00])
-    read2 = eeprom.read(0, 0, 8)
-
+    eeprom.write(0x00,0x00,[0xA0])
+    
+    read1 = eeprom.readout()
     print(read1)
-    print(read2)
 
 if __name__ == "__main__":
     main()
