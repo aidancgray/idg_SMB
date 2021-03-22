8d92005e925e8d466a73608b7e4e08123fe0ca88
aidancgray <aidancgray@gmail.com>
2021-03-22 21:27:43 +0000

    added some housekeeping to the beginning

--- python/main.py
+++ python/main.py
@@ -1,15 +1,43 @@
 #!/usr/local/bin/python3.8
 
 from EEPROM import EEPROM
+
+import logging
+import sys
 import time
+import RPi.GPIO as GPIO
+import spidev
+
+import EEPROM
+import GPIO_config
+import Gbl
+
+
+def runSMB(logLevel=logging.INFO):
+    logging.basicConfig(datefmt = "%Y-%m-%d %H:%M:%S",
+                        format = "%(asctime)s.%(msecs)03dZ %(name)-10s %(levelno)s %(filename)s:%(lineno)d %(message)s")
+    
+    logger = logging.getLogger('smb')
+    logger.setLevel(logLevel)
+    logger.info('starting logging')
+
+    # Read in EEPROM data
+    eeprom = EEPROM()
+
+    tlm = Gbl.telemetry
+
+    io = GPIO_config.io()
+    # reset both DACs
+    io.dac_reset(0)
+    time.sleep(0.001)
+    io.dac_reset(1)
+    time.sleep(0.001)
+
+    #io.dac_bank_sel(1)
+
 
 def main():
-    eeprom = EEPROM(0x50)
-
-    eeprom.write(0x00,0x00,[0xA0])
-    
-    read1 = eeprom.readout()
-    print(read1)
+    # runSMB(logging.INFO)
 
 if __name__ == "__main__":
     main()
