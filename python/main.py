61b71c50f3848ac48197327342bef9d5b6fa10da
aidancgray <aidancgray@gmail.com>
2021-05-04 15:19:27 +0100

    testing TCPip.py and cmdHandler.py

--- python/main.py
+++ python/main.py
@@ -6,15 +6,17 @@ import logging
 import sys
 import time
 import RPi.GPIO as GPIO
+import asyncio
 #import spidev
 
 import EEPROM
 import GPIO_config
 import Gbl
 from DAC8775 import DAC
+from TCPip import TCPServer
+from cmdHandler import CMDLoop
 
-
-def runSMB(logLevel=logging.INFO):
+async def runSMB(logLevel=logging.INFO):
     logging.basicConfig(datefmt = "%Y-%m-%d %H:%M:%S",
                         format = "%(asctime)s.%(msecs)03dZ %(name)-10s %(levelno)s %(filename)s:%(lineno)d %(message)s")
     
@@ -23,37 +25,41 @@ def runSMB(logLevel=logging.INFO):
     logger.info('starting logging')
 
     # Read in EEPROM data
-    # eeprom = EEPROM()
+    #eeprom = EEPROM()
 
-    tlm = Gbl.telemetry     # Telemetry dictionary
-    io = GPIO_config.io()   # GPIO pin configuration
+    #tlm = Gbl.telemetry     # Telemetry dictionary
+    #io = GPIO_config.io()   # GPIO pin configuration
     
-    dac0 = DAC(0, io)                   # initialize DAC0
-    dac0.dac_write_data(0x06, 0x000F) 
-    dac0.dac_write_data(0x07, 0x03C1)
-    dac0.dac_write_data(0x03, 0x01F0)
-    dac0.dac_write_data(0x04, 0x1005)
-    dac0.dac_write_data(0x05, 0x0000)
-    io.dac_ldac(1)
-    io.dac_ldac(0)
-    io.dac_ldac(1)
-    dac0.dac_write_data(0x03, 0x0020)
-    dac0.dac_write_data(0x05, 0xFFFF)
-    io.dac_ldac(1)
-    io.dac_ldac(0)
-    io.dac_ldac(1)
+    #dac0 = DAC(0, io, 'PID')    # initialize DAC0
 
-    readBack = dac0.dac_read_data(11)
+    # dac0.dac_write_data(0x06, 0x000F) 
+    # dac0.dac_write_data(0x07, 0x03C1)
+    # dac0.dac_write_data(0x03, 0x01F0)
+    # dac0.dac_write_data(0x04, 0x1005)
+    # dac0.dac_write_data(0x05, 0x0000)
+    # io.dac_ldac(1)
+    # io.dac_ldac(0)
+    # io.dac_ldac(1)
+    # dac0.dac_write_data(0x03, 0x0020)
+    # dac0.dac_write_data(0x05, 0xFFFF)
+    # io.dac_ldac(1)
+    # io.dac_ldac(0)
+    # io.dac_ldac(1)
+
+    # readBack = dac0.dac_read_data(11)
     
-    readBytes = readBack.to_bytes(3, byteorder='big')
-    for byte in readBytes:
-        print(format(byte,"08b"))
+    # readBytes = readBack.to_bytes(3, byteorder='big')
+    # for byte in readBytes:
+    #     print(format(byte,"08b"))
+    # uI = input("press any key to finish")
 
-    uI = input("press any key to finish")
+    tcpServer = TCPServer('', 9999)
+    cmdHandler = CMDLoop(tcpServer.qCmd, tcpServer.qXmit)
 
+    await asyncio.gather(tcpServer.start(), cmdHandler.start())
 
 def main():
-    runSMB(logging.INFO)
+    asyncio.run(runSMB(logging.INFO))
 
 if __name__ == "__main__":
     main()
